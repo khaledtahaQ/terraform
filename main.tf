0dbc2266ea88aaa8777048014f693d9e0f796197
khaled Taha <khaled.taha@quizplus.com>
2023-04-02 13:46:22 +0300

    pr test3

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
