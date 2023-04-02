86764bfd50554bda635ac054563ab5fc32010491
khaled Taha <khaled.taha@quizplus.com>
2023-04-02 15:08:31 +0300

    pr test3

--- main.tf
+++ main.tf
@@ -45,7 +45,6 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
   subnet_id = "subnet-0f5e8a6adba3ccd7f"
   user_data = <<-EOF
               #!/bin/bash
